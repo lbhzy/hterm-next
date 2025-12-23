b4d1790c590f6a2152bb9a4c0e8d7c26a8445088
lbhzy <296384478z@gmail.com>
2025-12-24 00:23:54 +0800

    抽出公共逻辑放入基类

--- hterm/channel/channel_pty.py
+++ hterm/channel/channel_pty.py
@@ -1,8 +1,10 @@
+import time
+import threading
 from PySide6.QtCore import Signal, QObject
 
 
 class PtyChannel(QObject):
-    """伪终端通道基类"""
+    """ 伪终端通道基类 """
     # 当从远端或本地PTY接收到数据时发射
     received = Signal(str)
     
@@ -11,31 +13,82 @@ class PtyChannel(QObject):
     
     # 当连接断开或发生错误时发射
     disconnected = Signal(str)
+
+    def __init__(self):
+        super().__init__()
+        self._running = False
+        self.is_connected = False
         
-    def connect(self) -> None:
+    def connect_impl(self) -> None:
+        """ 建立通道连接，需要子类实现 """
+        raise NotImplementedError
+
+    def disconnect_impl(self) -> None:
+        """ 断开通道连接，需要子类实现 """
+        raise NotImplementedError
+
+    def send_impl(self, data: str) -> None:
+        """ 向通道发送数据，需要子类实现 """
+        raise NotImplementedError
+    
+    def recv_non_blocking_impl(self, size: int) -> str | None:
         """
-        尝试建立连接（例如：启动SSH握手或打开串口）。
-        连接成功后，必须发射 connected 信号。
+        非阻塞模式从通道读取数据。
+        Returns:
+            str: 成功读取到的数据。如果没有可用数据应返回空字符串 ""。
+        Return:
+            None: 明确指示底层通道已关闭/断开，触发回收流程。
         """
         raise NotImplementedError
 
-    def disconnect(self) -> None:
-        """
-        主动断开连接。断开后，必须发射 signal_disconnected 信号。
-        """
-        raise NotImplementedError
-
-    def send_data(self, data: str) -> None:
-        """
-        将数据从终端模拟器发送到远端或本地PTY。
-        :param data: 待发送的原始字节数据 (例如：用户按下的按键)。
-        """
-        raise NotImplementedError
+    def send_window_size_impl(self, rows: int, cols: int) -> None:
+        """ 通知远端 PTY 窗口大小的改变，有些通道没有此功能可以不实现 """
+        pass
 
     def send_window_size(self, rows: int, cols: int) -> None:
-        """
-        通知远端PTY窗口大小的改变。
-        默认实现为空，因为并非所有通道（如Serial）都需要此功能。
-        子类可以重写此方法以实现具体的协议操作（如SSH的resize请求）。
-        """
-        pass
+        if self.is_connected:
+            self.send_window_size_impl(rows, cols)
+
+    def open(self):
+        try:
+            self.connect_impl()
+            # 启动数据接收线程
+            self._running = True
+            self._thread = threading.Thread(target=self.receive_loop, daemon=True)
+            self._thread.start()
+            # 更新状态
+            self.is_connected = True
+            self.connected.emit(f'连接成功')
+        except Exception as e:
+            self.disconnect_impl()
+            self.is_connected = False
+            self.disconnected.emit(f'连接失败：{e}')
+
+    def close(self):
+        if self.is_connected:
+            # 等待线程结束
+            self._running = False
+            self._thread.join()
+            # 断开通道连接
+            self.disconnect_impl()
+            # 更新状态
+            self.is_connected = False
+            self.disconnected.emit('已手动断开')
+
+    def send_data(self, data: str):
+        if self.is_connected:
+            self.send_impl(data)
+        else:
+            self.open()
+
+    def receive_loop(self):
+        while self._running:
+            time.sleep(0.01)
+            data = self.recv_non_blocking_impl(10000)
+            if data:
+                self.received.emit(data)
+            elif data == None:
+                self.disconnect_impl()
+                self._running = False
+                self.is_connected = False
+                self.disconnected.emit('已断开')
--- hterm/channel/channel_ssh.py
+++ hterm/channel/channel_ssh.py
@@ -16,66 +16,34 @@ class SshChannel(PtyChannel):
         self.username = username
         self.password = password
 
-        self._running = True
-        self.is_connected = False
-
         self.ssh = paramiko.SSHClient()
         # 允许连接不在 know_hosts 文件中的主机
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    def connect(self):
+    def connect_impl(self):
+        self.ssh.connect(self.server, self.port, self.username, self.password, timeout=1)
+        self.channel = self.ssh.invoke_shell()
+        self.channel.settimeout(0.01)   # 设置 recv 超时时间
+        self.transport = self.ssh.get_transport()
+        self.transport.set_keepalive(10)
+
+    def disconnect_impl(self):
+        self.ssh.close()
+
+    def send_impl(self, data: str):
+        data_bytes = data.encode()
+        self.channel.send(data_bytes)
+    
+    def send_window_size_impl(self, rows: int, cols: int):
+        self.channel.resize_pty(width=cols, height=rows)
+    
+    def recv_non_blocking_impl(self, size):
         try:
-            # 连接 ssh
-            self.ssh.connect(self.server, self.port, self.username, self.password, timeout=1)
-            self.channel = self.ssh.invoke_shell()
-            self.channel.settimeout(0.01)   # 设置 recv 超时时间
-            self.transport = self.ssh.get_transport()
-            self.transport.set_keepalive(10)
-            # 启动数据接收线程
-            self._running = True
-            self._thread = threading.Thread(target=self.receive_loop, daemon=True)
-            self._thread.start()
-            # 更新状态
-            self.is_connected = True
-            self.connected.emit(f'{self.server}:{self.port} 连接成功')
-        except Exception as e:
-            self.ssh.close()
-            self.is_connected = False
-            self.disconnected.emit(f'{self.server}:{self.port} 连接失败：{e}')
-
-    def disconnect(self):
-        if self.is_connected:
-            # 等待线程结束
-            self._running = False
-            self._thread.join()
-            # 关闭 ssh
-            self.ssh.close()
-            # 更新状态
-            self.is_connected = False
-            self.disconnected.emit('ssh 已手动断开')
-
-    def send_data(self, data: str):
-        if self.is_connected:
-            data_bytes = data.encode()
-            self.channel.send(data_bytes)
-        else:
-            self.connect()
-    
-    def send_window_size(self, rows: int, cols: int):
-        if self.is_connected:
-            self.channel.resize_pty(width=cols, height=rows)
-    
-    def receive_loop(self):
-        while self._running:
-            try:
-                data_bytes = self.channel.recv(10000)
-                if data_bytes:
-                    data_str = data_bytes.decode('utf-8', 'replace')
-                    self.received.emit(data_str)
-                else:   # 返回空代表通道已关闭
-                    self.ssh.close()
-                    self._running = False
-                    self.is_connected = False
-                    self.disconnected.emit('ssh 已断开')
-            except TimeoutError as e:
-                continue
+            data_bytes = self.channel.recv(size)
+            if data_bytes:
+                data_str = data_bytes.decode('utf-8', 'replace')
+                return data_str
+            else:
+                return None
+        except TimeoutError as e:
+            return ''
