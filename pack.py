4d655b2c2cd5f8d38a9539ea161eabaf07fcadf7
lbhzy <296384478z@gmail.com>
2025-12-25 23:49:47 +0800

    修改打包脚本

--- pack.py
+++ pack.py
@@ -5,7 +5,7 @@ def pack():
     params = [
         "hterm/main.py",  # 打包目标
         "-y",  # 不询问，直接替换旧成果物
-        # '-w',               # 不显示控制台
+        "-w",  # 不显示控制台
         "--specpath=build",  # 不需要 spec 文件，所以将其生成到 build 下
         "--name=hterm",  # 设置软件名
     ]
